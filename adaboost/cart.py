8e3283769f557cd1fc5c7d56ae29641a66dbc4f2
lizhishan <lizhishan@bytedance.com>
2018-05-03 20:02:56 +0800

    [va] add adaboost/

---
+++ adaboost/cart.py
@@ -0,0 +1,148 @@
+import pandas as pd
+import urllib
+import numpy as np
+from itertools import combinations
+import types
+import numbers
+
+def Gini(df, var, y):
+    '''
+    :param df:数据集，包含了需要计算Gini指数的特征和标签
+    :param var:需要计算Gini指数的特征
+    :param y: 标签，0或1
+    :return: Gini指数
+    '''
+    n = df.shape[0]
+    n1 = sum(df[y])
+    n2 = n - n1
+    return 1- (n1*1.0/n)**2-(n2*1.0/n)**2
+
+def Terminate(df, feature_list, y):
+    '''
+    :param df: 当前构建CART的数据集
+    :param feature_list: 当前构建CART的特征集
+    :param y: 标签
+    :return: 终止与否
+    '''
+    majorityClass = int(sum(df[y])*1.0/df.shape[0]>=0.5)
+    if feature_list == []:
+        return majorityClass
+    if df[feature_list].drop_duplicates().shape[0] == 1:
+        return majorityClass
+    if len(set(df[y])) == 1:
+        return majorityClass
+    return 'subTree'
+
+def FeatureType(df, var):
+    if isinstance(df.iloc[0][var],numbers.Real):
+        return 'numerical'
+    else:
+        return 'categorical'
+
+
+def NumFeatureSplit(df,var,y):
+    '''
+    :param df:
+    :param var:
+    :param y:
+    :return:
+    '''
+    x = sorted(list(set(df[var])))
+    split_gini = {}
+    N = df.shape[0]
+    for i in range(1,len(x)):
+        threshold = 0.5*(x[i-1]+x[i])
+        df_sub1 = df[df[var]<=threshold]
+        df_sub2 = df[df[var] > threshold]
+        n1, n2 = df_sub1.shape[0], df_sub2.shape[0]
+        varGini = n1*1.0/N*Gini(df_sub1, var, y) + n2*1.0/N*Gini(df_sub2, var, y)
+        split_gini[threshold] = varGini
+    return {'splittedPoint' : min(split_gini,key=split_gini.get), 'minGini' : min(split_gini.values())}
+
+
+def CatFeatureSplit(df,var,y):
+    '''
+    :param df:
+    :param var:
+    :param y:
+    :return:
+    '''
+    avgLabel = df.groupby([var])[y].mean().to_frame()
+    avgLabelSorted = avgLabel.sort_values(by=y)
+    CatVar = list(avgLabelSorted.index)
+    Cat2Num = {CatVar[i]:i for i in range(len(CatVar))}
+    df['temp_col'] = df[var].map(Cat2Num)
+    splitNumerical = NumFeatureSplit(df, 'temp_col', y)
+    splitted_new_var = [k for k in Cat2Num.keys() if Cat2Num[k]<=splitNumerical['splittedPoint']]
+    return {'splittedPoint':splitted_new_var,'minGini':splitNumerical['minGini']}
+
+def TrainCART(df,feature_list,y, depth = 10000):
+    '''
+    :param df: 建立CART的数据集
+    :param feature_list: 用于模型开发的特征的列表
+    :param y: 标签，取值0，1
+    :param depth: 树的深度。当指明深度时，设置该参数。
+    :return: 叶子节点，或者子树。最终返回字典形式的CART树
+    '''
+    child = Terminate(df, feature_list, y)
+    majorityClass = int(sum(df[y]) * 1.0 / df.shape[0] >= 0.5)
+    if child in [0,1]:
+        return child
+    if depth == 0:
+        return majorityClass
+    featureGini = {}
+    for feature in feature_list:
+        if len(set(df[feature])) == 1:
+            return majorityClass
+        if FeatureType(df,feature) == 'numerical':
+            featureSplit = NumFeatureSplit(df,feature,y)
+        else:
+            featureSplit = CatFeatureSplit(df, feature, y)
+        featureGini[feature] = [featureSplit['splittedPoint'],featureSplit['minGini']]
+    sortedFeatureGini = sorted(featureGini.items(),key=lambda x: x[1][1])
+    bestFeature, bestSplit = sortedFeatureGini[0][0], sortedFeatureGini[0][1][0]
+
+    cartResult = {bestFeature:{}}
+    if type(bestSplit) == types.ListType:
+        subTreeFeatures = [i for i in feature_list if i != bestFeature]
+        bestFeaturesVals = list(set(df[bestFeature]))
+
+
+        leftTree = df.loc[df[bestFeature].isin(bestSplit)]
+        leftVals = bestSplit
+
+        cartResult[bestFeature][str(leftVals)] = TrainCART(leftTree,subTreeFeatures,y,depth-1)
+
+        rightTree = df.loc[~df[bestFeature].isin(bestSplit)]
+        rightVals = [i for i in bestFeaturesVals if i not in set(leftVals)]
+        cartResult[bestFeature][str(rightVals)] = TrainCART(rightTree, subTreeFeatures, y,depth-1)
+    else:
+        subTreeFeatures = [i for i in feature_list if i != bestFeature]
+
+        leftTree = df.loc[df[bestFeature]<=bestSplit]
+
+        cartResult[bestFeature]["<="+str(bestSplit)] = TrainCART(leftTree, subTreeFeatures, y,depth-1)
+
+        rightTree = df.loc[df[bestFeature]>bestSplit]
+        cartResult[bestFeature][">"+str(bestSplit)] = TrainCART(rightTree, subTreeFeatures, y,depth-1)
+
+    return cartResult
+
+
+def predCART(record, cart):
+    root, subTree = cart.items()[0]
+    for k, v in subTree.items():
+        if type(v).__name__ == 'dict':
+            return predCART(record, v)
+        else:
+            if k.find('>') > -1 or k.find('<=') > -1:
+                if eval(str(record[root])+k):
+                    return v
+                else:
+                    return 1-v
+            else:
+                if record[root] in eval(k):
+                    return v
+                else:
+                    return 1-v
+
---
+++ adaboost/demo/AdaBoost.py
@@ -0,0 +1,134 @@
+# encoding: utf-8
+
+import pandas as pd
+import numpy as np
+import random
+from sklearn import model_selection
+import cart as CART
+
+
+def RepeatSample(df, w):
+    '''
+    :param df:
+    :param w:
+    :return:
+    '''
+    w_min = min(w)
+    sampleFreq = [int(np.ceil(i/w_min)) for i in w]
+    df2 = pd.Series()
+    for i in range(df.shape[0]):
+        freq = sampleFreq[i]
+        for j in range(1,freq+1):
+            df2 = pd.concat([df2,df.iloc[i]],axis = 1)
+    df2 = df2.T
+    df2.index = range(df2.shape[0])
+    df2 = df2.drop(0)
+    df2.index = range(df2.shape[0])
+    return df2
+
+def UpdateWeights(real,pred, w0, alpha):
+    '''
+    :param real:
+    :param pred:
+    :param w0:
+    :return:
+    '''
+    if abs(real-pred)<0.0001:
+        w1 = w0*np.exp(-alpha)
+    else:
+        w1 = w0*np.exp(alpha)
+    return w1
+
+def SimpleClassiferPred(df,ID,pred):
+    '''
+    :param df:带有预测值的数据集，其中每个样本可能 会有重复记录
+    :param ID: 样本的唯一标识
+    :param pred:样本的预测。同一样本可能会有不同的预测
+    :return: 对重复样本，返回出现频率最高的预测值
+    '''
+    idPred = df.groupby([ID])[pred].mean()
+    idPredDict = idPred.to_dict()
+    idPredDict = {k:int(round(v)) for k,v in idPredDict.items()}
+    return idPredDict
+
+def  TrainAda(df, feature_list, iteration, y,featureSampling=False, treeDepth=3):
+    '''
+    :param df:训练集，包含特征和标签
+    :param feature_list:特征集
+    :param iteration: 迭代次数
+    :param featureSampling: 从特征集中的抽样率。默认不进行特征抽样
+    :param treeDepth每棵树的深度
+    :return:每次迭代的权重和CART模型
+    '''
+    N = df.shape[0]
+    w0 = np.array([1.0]*N)/N
+    alpha_list = []
+    cart_list = []
+    df2 = df.copy()
+    df2['ID'] = range(df.shape[0])
+    df2['w0'] = w0
+    for k in range(iteration):
+        if not featureSampling:
+            subFeatures = feature_list
+        else:
+            samplingSize = int(len(feature_list)*featureSampling)
+            subFeatures = random.sample(feature_list, samplingSize)
+        trainData = RepeatSample(df2, w0)
+        trainData[y] = trainData[y].astype(float)
+        cart = CART.TrainCART(trainData,subFeatures,y,treeDepth)
+        trainData['pred'] = trainData[subFeatures].apply(lambda x: CART.predCART(x, cart), axis=1)
+        predicion = SimpleClassiferPred(trainData,'ID','pred')
+        df2['pred'] = df2['ID'].map(predicion)
+        err_pred = trainData.apply(lambda x: abs(x[y] - x.pred), axis=1)
+        err = sum(err_pred)/N
+        if err >= 0.5:   #预测错误率超过0.5的分类器进行舍弃
+            continue
+        alpha = 0.5*np.log((1-err)/err)
+        cart_list.append(cart)
+        alpha_list.append(alpha)
+        w2 = df2.apply(lambda x: UpdateWeights(x[y], x.pred, x.w0, alpha),axis = 1)
+        w0 = [i/sum(w2) for i in w2]
+        df2['w0'] = w0
+        if err<=0.00001:
+            break
+    return {'alpha':alpha_list, 'cart model':cart_list}
+
+
+bank = 'bank.csv'
+AllData = pd.read_csv(bank, header=0)
+Attributes = list(AllData.columns)
+Attributes.remove('y')
+AllData['y2'] = AllData['y'].apply(lambda x: int(x == 'yes'))
+#fewerAttributes = [u'age', u'job', u'marital', u'education', u'default', u'cash_loan', u'contact_number_type', u'maturity']
+
+smallData = AllData[Attributes+['y2']]
+
+df1 = smallData.loc[smallData['y2'] == 1]
+df0 = smallData.loc[smallData['y2'] == 0]
+df0_2 = df0.iloc[0:1000]
+myData= pd.concat([df0_2,df1])
+
+X_train,X_test = model_selection.train_test_split(myData,test_size=0.4, random_state=0)
+
+x1 = X_train.copy()
+x2 = X_test.copy()
+cart = CART.TrainCART(x1,Attributes,'y2',9)
+x2['y3'] = x2[Attributes].apply(lambda x: CART.predCART(x, cart), axis=1)
+err_pred = x2.apply(lambda x: abs(x.y2 - x.y3), axis=1)
+err = sum(err_pred)*1.0/len(err_pred)
+print err   #0.597701149425
+
+
+T = 10
+AdaModel = TrainAda(X_train, Attributes, T,'y2')
+alpha = AdaModel['alpha']
+cart_list = AdaModel['cart model']
+pred = X_test[Attributes].apply(lambda x: CART.predCART(x, cart_list[0])*alpha[0], axis=1)
+for k in range(1,T):
+    pred2 = X_test[Attributes].apply(lambda x: CART.predCART(x, cart_list[k])*alpha[k], axis=1)
+    pred = pd.concat([pred,pred2],axis=1)
+X_test['ada'] = pred.apply(sum,axis=1)
+X_test['ada'] = X_test['ada'].map(lambda x: int(x>0.5))
+err_pred = X_test.apply(lambda x: abs(x.y2 - x.ada), axis=1)
+err = sum(err_pred)*1.0/len(err_pred)
+print err #  0.40229885057471265
